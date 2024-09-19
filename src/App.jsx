e150067c86209dac74006391889a4b67d1857687
RaulGM3 <apaladario@gmail.com>
2024-09-19 11:33:52 +0200

    Add .gitignore file and ignore logs, editor files, and build artifacts
    Create a new .gitignore file to exclude logs, editor directories and files, and build artifacts from version control.
    
    Add project creation functionality
    
    Implement the ability to create new projects in the App component. This includes handling the start and cancel of project creation, as well as saving the new project data.
    
    Add Button component
    
    Create a new Button component that renders a styled button element.
    
    Add Input component
    
    Create a new Input component that renders an input or textarea element with styling.
    
    Add Modal component
    
    Implement a Modal component that can be used to display a dialog with custom content.
    
    Add NewProject component
    
    Create a NewProject component that allows users to enter details for a new project, including title, description, and due date.
    
    Add NoProjectSelected component
    
    Implement a NoProjectSelected component that is displayed when no project is selected. It provides a button to create a new project.
    
    Add ProjectSidebar component
    
    Create a ProjectSidebar component that displays a list of projects and allows users to add new projects.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,62 @@
+import { useState } from "react";
+
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState (prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  function handleCancelAddProject () {
+    setProjectsState (prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined
+      }
+    })
+  }
+
+  function handleAddProject (projectData) {
+    setProjectsState (prevState => {
+      const newProject = {
+        ...projectData, 
+        id: prevState.projects.length
+      }
+      // console.log (newProject)
+      return {
+        ...prevState, // que pasa si lo quitamos?
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
+  } else {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      {/* <NewProject /> */}
+      {content}
+    </main>
   );
 }
 
